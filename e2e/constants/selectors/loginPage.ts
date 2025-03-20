158314dc8cae9cc68d627ee8d93c49ceeec30be8
Ashutosh Panda <ashutosh.panda@pixelcompute.com>
2025-03-20 15:53:44 +0530

    Logged into the website and completed the global setup

---
+++ e2e/constants/selectors/loginPage.ts
@@ -0,0 +1,6 @@
+export const LOGIN_SELECTORS = {
+  loginEmailField: 'login-email-text-field',
+  loginPasswordField: 'login-password-text-field',
+  submitButton: 'login-submit-button',
+  profileAvatar: 'profile-avatar',
+}
---
+++ e2e/pom/loginPage.ts
@@ -0,0 +1,22 @@
+import {expect, Page} from "@playwright/test";
+import { STORAGE_STATE } from "../constants/common";
+import { LOGIN_SELECTORS } from "../constants/selectors/loginPage";
+
+
+export class LoginPage {
+
+  constructor(private page: Page) {}
+
+  loginToWebsite= async() => {
+    await this.page.goto("/");
+    await this.page.getByTestId(LOGIN_SELECTORS.loginEmailField).fill(process.env.LOGIN_EMAIL!);
+    await this.page.getByTestId(LOGIN_SELECTORS.loginPasswordField).fill(process.env.LOGIN_PASSWORD!);
+    await this.page.getByTestId(LOGIN_SELECTORS.submitButton).click();
+    await this.page.context().storageState({ path: STORAGE_STATE });
+  }
+
+  verifyLogin = expect(this.page.getByTestId(LOGIN_SELECTORS.profileAvatar)).toBeVisible;
+
+  saveStorageState = this.page.context().storageState({ path: STORAGE_STATE });
+
+}
